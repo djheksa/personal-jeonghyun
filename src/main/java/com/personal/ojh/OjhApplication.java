9fb831598d0fd386ebba6552c372d168f9e90de6
ojh <djheksa@gmail.com>
2025-03-16 22:44:17 +0900

    Initial commit

---
+++ src/main/java/com/personal/ojh/OjhApplication.java
@@ -0,0 +1,13 @@
+package com.personal.ojh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/ojh/OjhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.ojh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
